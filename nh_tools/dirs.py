57f3d9880281d50d707f77194a0f42f864c94551
nikohonu <nikohonu@gmail.com>
2022-02-07 23:11:52 +0200

    add get_data_path() to dirs.py & and rename method in dirs.py

--- nh_tools/dirs.py
+++ nh_tools/dirs.py
@@ -1,5 +1,6 @@
-from pathlib import Path
 from os import getenv
+from pathlib import Path
+
 from nh_tool.file import open_file
 
 
@@ -12,26 +13,27 @@ def get_path_from_env(var: str, default: Path):
     return path
 
 
-def get_path_from_user_dirs(var: str, default: Path):
-    data = open_file(get_config() / 'user-dirs.dirs')
-    user_dirs = {}
-    for line in [line for line in data.split('\n') if not line.startswith('#') and line]:
-        key, value = line.split('=')
-        value = value.replace('$HOME', str(get_home())).replace('"', '')
-        user_dirs[key] = Path(value)
-    return user_dirs[var] if var in user_dirs else default
-
-
 def get_home():
     return Path.home()
 
 
-def get_config():
+def get_config_path():
     return get_path_from_env('XDG_CONFIG_HOME', get_home() / '.config')
 
 
-def get_music_dir():
-    return get_path_from_user_dirs('XDG_MUSIC_DIR', get_home() / 'Music')
+def get_data_path():
+    return get_path_from_env('XDG_DATA_HOME', get_home() / '.local' / 'share')
 
-def get_archive_dir():
-    return get_home() / 'archive'
+
+def get_path_from_user_dirs(var: str, default: Path):
+    data = open_file(get_config_path() / 'user-dirs.dirs')
+    user_dirs = {}
+    for line in [line for line in data.split('\n') if not line.startswith('#') and line]:
+        key, value = line.split('=')
+        value = value.replace('$HOME', str('~'))
+        user_dirs[key] = Path(value).expanduser()
+    return user_dirs[var] if var in user_dirs else default
+
+
+def get_music_path():
+    return get_path_from_user_dirs('XDG_MUSIC_DIR', get_home() / 'Music')
